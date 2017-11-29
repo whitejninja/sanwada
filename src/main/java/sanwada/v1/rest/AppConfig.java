7eada05beca25a9aad430ea8ddbca802cab559c7
Kalpa Gunarathna <chathuranga456@gmail.com>
2017-11-29 19:52:57 +0530

    Add swagger configuration

--- src/main/java/sanwada/v1/rest/AppConfig.java
+++ src/main/java/sanwada/v1/rest/AppConfig.java
@@ -2,6 +2,8 @@ package sanwada.v1.rest;
 
 import javax.ws.rs.core.Application;
 
+import io.swagger.jaxrs.config.BeanConfig;
+
 import java.util.HashSet;
 import java.util.Set;
 
@@ -10,12 +12,27 @@ import javax.ws.rs.ApplicationPath;
 @ApplicationPath(value = "/v1")
 public class AppConfig extends Application {
 
+    public AppConfig() {
+        BeanConfig beanConfig = new BeanConfig();
+        beanConfig.setVersion("v1");
+        beanConfig.setSchemes(new String[]{"http"});
+        beanConfig.setHost("localhost:8080");
+        beanConfig.setBasePath("/v1/sanwada");
+        beanConfig.setResourcePackage(QuestionResource.class.getPackage().getName());
+        beanConfig.setScan(true);
+    }
+
     @Override
     public Set<Class<?>> getClasses() {
 
         final Set<Class<?>> classes = new HashSet<>();
         // register root resource classes
+		classes.add(QuestionResource.class);
         classes.add(QuestionResource.class);
+
+        // Swagger core providers
+        classes.add(io.swagger.jaxrs.listing.ApiListingResource.class);
+        classes.add(io.swagger.jaxrs.listing.SwaggerSerializers.class);
         return classes;
 
     }
