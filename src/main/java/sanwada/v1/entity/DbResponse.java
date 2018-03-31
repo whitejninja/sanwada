1fde6f9872822f5dd35d8b6089957bee31173675
Kalpa Gunarathna <chathuranga456@gmail.com>
2018-03-31 20:39:02 +0530

    Add javadoc

--- src/main/java/sanwada/v1/entity/DbResponse.java
+++ src/main/java/sanwada/v1/entity/DbResponse.java
@@ -2,9 +2,21 @@ package sanwada.v1.entity;
 
 import sanwada.v1.dao.DbOperationStatus;
 
+/**
+ * Data transfer object between REST and data layers
+ */
 public class DbResponse{
 
+  /**
+   * Status of the database operation executed.
+   */
   private DbOperationStatus status;
+
+  /**
+   * Result after the database operation.
+   * In the case of a put operation on database, result will be the modified object.
+   * In the case of a get operation on database result will be object retrieved.
+   */
   private Object result;
 
   public DbResponse(DbOperationStatus status, Object result) {
